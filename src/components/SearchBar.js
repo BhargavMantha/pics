f30a52af5dca63cd135eeee8942b7578cb2b38ea
BhargavMantha <manthabhargav@gmail.com>
2021-12-21 16:00:13 +0530

    image search component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    const { term } = this.state;
+    this.props.onSubmit(term);
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={({ target: { value } }) =>
+                this.setState({ term: value })
+              }
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
